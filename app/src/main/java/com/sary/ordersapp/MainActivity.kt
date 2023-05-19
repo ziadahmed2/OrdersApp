eb94f93718baa7f56e8c8b80fdbab590509dd8ff
zahmed <zahmed@kashier.io>
2023-05-19 19:55:56 +0300

    remove log

--- app/src/main/java/com/sary/ordersapp/MainActivity.kt
+++ app/src/main/java/com/sary/ordersapp/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.sary.ordersapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.sary.core_domain.preferences.Preferences
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@AndroidEntryPoint
 class MainActivity: AppCompatActivity() {
   
   @Inject
@@ -20,10 +13,6 @@ class MainActivity: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    CoroutineScope(Dispatchers.Main).launch {
-      delay(3000)
-      Log.d("TAG", "onCreate: ${prefs.getLanguage()}")
-    }
     
   }
 }
