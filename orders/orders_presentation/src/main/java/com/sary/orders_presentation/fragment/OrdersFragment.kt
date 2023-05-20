642bf5f2ea7eca6e5f6a754d06b36c52b7f684ae
zahmed <zahmed@kashier.io>
2023-05-20 03:35:45 +0300

    added extensions and setup toolbar

--- orders/orders_presentation/src/main/java/com/sary/orders_presentation/fragment/OrdersFragment.kt
+++ orders/orders_presentation/src/main/java/com/sary/orders_presentation/fragment/OrdersFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.sary.core_presentation.extensions.setupToolbar
 import com.sary.orders_presentation.databinding.FragmentOrdersBinding
 
 class OrdersFragment: Fragment() {
@@ -19,6 +20,18 @@ class OrdersFragment: Fragment() {
   
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    setupViews()
+  }
+  
+  private fun setupViews() {
+    setupToolbar()
+  }
+  
+  private fun setupToolbar() = binding.header.setupToolbar(Pair(false, "Orders")) {
+    activity?.setActionBar(it.toolbar)
+    activity?.actionBar?.setDisplayShowTitleEnabled(false)
+    activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
+    it.toolbar.setNavigationOnClickListener { activity?.finish() }
   }
   
   override fun onDestroyView() {
