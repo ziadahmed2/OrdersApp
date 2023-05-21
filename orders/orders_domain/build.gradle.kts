a13426eeba5ff77c2f8173f05822df47575b7c04
zahmed <zahmed@kashier.io>
2023-05-21 08:26:13 +0300

    repo unit tests

--- orders/orders_domain/build.gradle.kts
+++ orders/orders_domain/build.gradle.kts
@@ -1,8 +1,16 @@
 apply {
-    from("$rootDir/base-module.gradle")
+  from("$rootDir/base-module.gradle")
 }
 
 dependencies {
-    "implementation"(project(Modules.coreDomain))
-    "implementation"(Coroutines.coroutines)
+  "implementation"(project(Modules.coreDomain))
+  "implementation"(Coroutines.coroutines)
+  
+  "testImplementation"("junit:junit:4.13.2")
+  "testImplementation"("org.mockito:mockito-core:4.0.0")
+  "testImplementation"("android.arch.core:core-testing:1.1.1")
+  "testImplementation"("com.google.truth:truth:1.1.3")
+  "testImplementation"("com.google.dagger:hilt-android-testing:2.45")
+  "androidTestImplementation"("com.google.dagger:hilt-android-testing:2.45")
+  "kaptTest"("com.google.dagger:hilt-android-compiler:2.45")
 }
